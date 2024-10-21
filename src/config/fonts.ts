d354a6bbf82631804064114e411608d4207e8fb6
Devcer <devcer.cs@gmail.com>
2024-10-21 05:26:22 -0500

    config fonts and global styles

---
+++ src/config/fonts.ts
@@ -0,0 +1,8 @@
+import { Poppins } from 'next/font/google'
+
+export const poppins = Poppins({ subsets: ['latin'], weight: ['300'] })
+
+export const titleFont = Poppins({
+    subsets: ['latin'],
+    weight: ['500', '700']
+})
